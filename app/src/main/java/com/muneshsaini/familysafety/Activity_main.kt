9d3aea47415a069e06e7e0b7d1d2b1ae1b1657b2
Munesh Saini <136725215+MuneshSaini@users.noreply.github.com>
2024-02-23 21:42:13 +0530

    init commit

--- app/src/main/java/com/muneshsaini/familysafety/Activity_main.kt
+++ app/src/main/java/com/muneshsaini/familysafety/Activity_main.kt
@@ -1,34 +1,64 @@
 package com.muneshsaini.familysafety
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Activity_main : AppCompatActivity() {
+
+
+   val permissions = arrayOf(
+
+       android.Manifest.permission.ACCESS_FINE_LOCATION,
+       android.Manifest.permission.ACCESS_COARSE_LOCATION
+   )
+    val PermissionCode = 78
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        askForPermission()
 
         var Bottombar = findViewById<BottomNavigationView>(R.id.bottombar)
         Bottombar.setOnItemSelectedListener {
 
-            if(it.itemId == R.id.nav_home){
-                inflateFragment(HomeFragment.newInstance())
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    inflateFragment(HomeFragment.newInstance())
+                }
             }
             if(it.itemId == R.id.nav_guard){
                 inflateGuard()
             }
             if (it.itemId == R.id.nav_dashboard){
-                inflateFragment(Dashboard_Fragment.newInstance())
+                inflateFragment(MapsFragment())
             }
             if(it.itemId == R.id.nav_profile){
                 inflateFragment(Profile_Fragment.newInstance())
             }
               true
           }
-
+          Bottombar.selectedItemId = R.id.nav_home
         }
+
+    private fun askForPermission() {
+        ActivityCompat.requestPermissions(this,permissions,PermissionCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    { super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PermissionCode) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText( this,  "Permission Granted", Toast.LENGTH_SHORT).show()
+            else askForPermission()
+        }
+    }
 
     private fun inflateFragment(newInstance: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
